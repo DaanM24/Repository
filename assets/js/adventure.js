b1cc05a5e3aaf875cf76c9a8b2e02920df0bd7b8
Daan <99054517@mydavinci.nl>
2019-11-27 09:58:50 +0100

    Was niet vergeten

--- assets/js/adventure.js
+++ assets/js/adventure.js
@@ -2,32 +2,27 @@ var button1 = document.getElementById("button1");
 var button2 = document.getElementById("button2");
 var button3 = document.getElementById("button3");
 
+
+
+document.getElementById("inventoryItem").style.display = 'none';
+document.getElementById("title").innerHTML = "Squirrel man's adventure";
+document.getElementById("description").innerHTML = "Welcome to the gun show. Kies je keuze om een keuze te maken.";
+
+document.getElementById("button2").style.display = 'none';
+document.getElementById("button3").style.display = 'none';
+
 function start(){
-    
+    document.getElementById("description").innerHTML = "Je word wakker in een bos";
+    document.getElementById("button1").innerHTML = "Rick";
+    var image = document.createElement("Forest");
+    image.setAttribute("src", "assets/img/Forest.jpg");
+    image.setAttribute("alt", "Forest");
+    image.setAttribute("width", "304");
+    image.setAttribute("height", "228");
+    document.body.appendChild(image);
 }
 
-function rick(){
-    document.getElementById("description").innerHTML = "Rick is uber cool";
-    document.getElementById("button2").style.display = 'none';
-    document.getElementById("button3").style.display = 'none';
-}
 
-function rick2(){
-    document.getElementById("description").innerHTML = "Is Rick eigenlijk wel cool?";
-    document.getElementById("button1").style.display = 'none';
-    document.getElementById("button3").style.display = 'none';
-}
+document.getElementById("button1").innerHTML = "Start game";
 
-function rick3(){
-    document.getElementById("description").innerHTML = "Rick is zeer cool";
-    document.getElementById("button1").style.display = 'none';
-    document.getElementById("button2").style.display = 'none';
-}
-
-document.getElementById("button1").innerHTML = "Leuke verassing";
-document.getElementById("button2").innerHTML = "Geen leuke verassing";
-document.getElementById("button3").innerHTML = "De beste verassing";
-
-document.getElementById("button1").onclick = function() {rick()};
-document.getElementById("button2").onclick = function() {rick2()};
-document.getElementById("button3").onclick = function() {rick3()};
+document.getElementById("button1").onclick = function() {start()};
